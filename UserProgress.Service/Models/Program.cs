d99e98ff40c390df3573f9de880e272b03ac115d
Chris Faria <chrisfaria@gmail.com>
2020-04-17 18:01:42 -0400

    create progress in CosmosDB

---
+++ UserProgress.Service/Models/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserProgress.Service.Models
+{
+    public class Program
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ UserProgress.Service/UserProgressAPI.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using UserProgress.Service.Models;
+
+namespace UserProgress.Service
+{
+    public static class UserProgressAPI
+    {
+        [FunctionName("CreateUserProgram")]
+        public static async Task<IActionResult> CreateUserProgram(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "progress/program")] HttpRequest req,
+            [CosmosDB(
+                databaseName: "WorkoutApp",
+                collectionName: "UserPrograms",
+                ConnectionStringSetting = "AzureWebJobsStorage")]
+                IAsyncCollector<EnrolledProgramTableEntity> userProgramOut,
+            ILogger log)
+        {
+            log.LogInformation("Create a new user program");
+
+            // Read the request body
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var input = JsonConvert.DeserializeObject<EnrolledProgramCreateModel>(requestBody);
+
+            var userprogram = new EnrolledProgramTableEntity()
+            {
+                UserNamen = input.UserName,
+                Programs = input.Programs,
+                Status = input.Status
+            };
+
+            await userProgramOut.AddAsync(userprogram);
+
+            return new OkObjectResult(userprogram);
+        }
+    }
+}
